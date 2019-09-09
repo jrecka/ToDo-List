cd37019a4aadb56a17b9b501cdac9d8cc639b2cf
Jola <recka.jolanta@gmail.com>
2019-09-09 15:44:25 +0200

    Updates

---
+++ precache-manifest.ed64a92e9dc5d6972bc46057248ae14d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d917e95847a044b0fdd63e9e8ce9b1b",
+    "url": "/ToDo-List/index.html"
+  },
+  {
+    "revision": "8c7ac61d03889dc42c39",
+    "url": "/ToDo-List/static/css/main.5aed9a8b.chunk.css"
+  },
+  {
+    "revision": "d44aaea38d43936fbadb",
+    "url": "/ToDo-List/static/js/2.3676f36b.chunk.js"
+  },
+  {
+    "revision": "8c7ac61d03889dc42c39",
+    "url": "/ToDo-List/static/js/main.d7a29115.chunk.js"
+  },
+  {
+    "revision": "aa01cae781a8458028e2",
+    "url": "/ToDo-List/static/js/runtime~main.d8d2c0bc.js"
+  }
+]);
